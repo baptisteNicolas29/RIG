d6fc1e2117162cbcf5b6f4c62cfb51554a07410d
Baptiste <baptiste.nicolas@protonmail.com>
2023-03-14 00:46:56 +0100

    remove Settings

--- Settings/ModuleSettings.py
+++
@@ -1,7 +0,0 @@
-class ModuleSettings:
-
-    def __init__(self, blackbox=[], attr=[], part=[]):
-
-        self.blackbox = blackbox
-        self.attr = attr
-        self.part = part
--- Settings/__init__.py
+++
@@ -1,2 +0,0 @@
-from .Settings import Settings
-from .ModuleSettings import ModuleSettings
